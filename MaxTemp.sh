e255cd11de0d61c3085d972fe5382ecf447a98dc
Harlan Gough <goughh@i21a-s1.ufhpc>
2016-11-21 09:29:36 -0500

    Adding Assignment 1.2

---
+++ MaxTemp.sh
@@ -0,0 +1,13 @@
+### grabing yesterdays weather
+
+#!/bin/bash
+
+a=`date -d "yesterday" "+%Y/%m/%d"`
+
+echo $a
+
+curl "https://www.wunderground.com/history/airport/GNV/$a/DailyHistory.heml?&format=1" > gnv.txt
+
+maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
+
+echo Yesterdays Max Temp was $maxTemp
